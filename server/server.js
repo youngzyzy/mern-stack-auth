0c5aefd9a297c3e99f37f5fe979374968d28e069
youngzyzy <zyrrellibrodo@yahoo.com>
2025-02-28 21:33:00 -0800

    initializing repository w/ code

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+
+app.use(cookieParser);
+
+app.use(cors({ credentials: true }));
+
+app.listen(port, () =>
+  console.log(`Server started listening on PORT: ${port}`)
+);
